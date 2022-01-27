6f5844e2054d91a11cd6280e0227b05481a0567d
alumno <asa@g.com>
2022-01-27 21:34:37 +0100

    Modificar archivo BarajaEspañola.java

--- BarajaEspañola.java
+++ BarajaEspañola.java
@@ -1,7 +1,33 @@
 package ejerciciosColeccionesDiccionarios;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class BarajaEspañola {
     public static void main(String[] args) {
+        HashMap<String, String> conjuntoBaraja = new HashMap<String, String>();
+        ArrayList<String> nombreBaraja = new ArrayList<String>();
+        ArrayList<String> valorBaraja = new ArrayList<String>();
+
+        valorBaraja.add("as");
+        valorBaraja.add("dos");
+        valorBaraja.add("tres");
+        valorBaraja.add("cuatro");
+        valorBaraja.add("cinco");
+        valorBaraja.add("seis");
+        valorBaraja.add("siete");
+        valorBaraja.add("ocho");
+        valorBaraja.add("nueve");
+        valorBaraja.add("sota");
+        valorBaraja.add("Caballo");
+        valorBaraja.add("Rey");
+
+        nombreBaraja.add("oros");
+        nombreBaraja.add("copas");
+        nombreBaraja.add("espadas");
+        nombreBaraja.add("bastos");
+
         
+
     }
 }
