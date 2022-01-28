bca955b9f958de18cf333e7347177313b1008ce4
alumno <asa@g.com>
2022-01-28 18:02:03 +0100

    Terminar el programa BarajaEspañola

--- BarajaEspañola.java
+++ BarajaEspañola.java
@@ -2,32 +2,46 @@ package ejerciciosColeccionesDiccionarios;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Map;
 
 public class BarajaEspañola {
     public static void main(String[] args) {
-        HashMap<String, String> conjuntoBaraja = new HashMap<String, String>();
         ArrayList<String> nombreBaraja = new ArrayList<String>();
-        ArrayList<String> valorBaraja = new ArrayList<String>();
 
-        valorBaraja.add("as");
-        valorBaraja.add("dos");
-        valorBaraja.add("tres");
-        valorBaraja.add("cuatro");
-        valorBaraja.add("cinco");
-        valorBaraja.add("seis");
-        valorBaraja.add("siete");
-        valorBaraja.add("ocho");
-        valorBaraja.add("nueve");
-        valorBaraja.add("sota");
-        valorBaraja.add("Caballo");
-        valorBaraja.add("Rey");
+        HashMap<Integer, String> valorBaraja = new HashMap<Integer, String>();
+
+        valorBaraja.put(1, "as");
+        valorBaraja.put(2, "dos");
+        valorBaraja.put(3, "tres");
+        valorBaraja.put(4, "cuatro");
+        valorBaraja.put(5, "cinco");
+        valorBaraja.put(6, "seis");
+        valorBaraja.put(7, "siete");
+        valorBaraja.put(8, "ocho");
+        valorBaraja.put(9, "nueve");
+        valorBaraja.put(10, "Sota");
+        valorBaraja.put(11, "Caballo");
+        valorBaraja.put(12, "Rey");
 
         nombreBaraja.add("oros");
         nombreBaraja.add("copas");
         nombreBaraja.add("espadas");
         nombreBaraja.add("bastos");
 
-        
+        int contador = 0;
+
+
+        for (int i=0; i<5; i++){
+            int randomCartaValor = (int) (1+Math.random()*11+1);
+            System.out.print(valorBaraja.get(randomCartaValor) + " ");
+            contador = contador + randomCartaValor;
+
+            int randomCartaNombre = (int) (Math.random()*4);
+            System.out.println(nombreBaraja.get(randomCartaNombre));
+        }
+
+        System.out.println("El valor de las cartas es: " + contador);
+
 
     }
 }
